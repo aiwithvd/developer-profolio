81dc58180745efb6343de0fa9a7e458df4920474
Vishvdeep Dasadiya <vishvdeep@7span.com>
2024-11-09 13:57:34 +0530

    transform jpg file to png file

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
     name: 'Vishvdeep Dasadiya',
     title: "AI Engineer",
     desciption:"I specialize in Generative AI, Data Science, and Machine Learning to create innovative and efficient solutions. With expertise in MLOps, I streamline the deployment and management of scalable AI models.",
-    image: '../assets/png/IMG_8106.jpg',
+    image: '../assets/png/IMG_8106.png',
     resumePdf: resume
 }
 
